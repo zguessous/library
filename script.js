e3221c2f99803ccfea13af98c605e0f23d55e472
Zineb Guessous <zineb.gues@gmail.com>
2024-08-25 21:23:00 +0100

    'add book' feature done

--- script.js
+++ script.js
@@ -12,7 +12,11 @@ const cancelBtn = newBookDialog.querySelector("#cancelBtn");
 const allBooksNmbr = document.querySelector("#allBooksNmbr");
 const booksReadNmbr = document.querySelector("#booksReadNmbr");
 
+const mainSection = document.querySelector("main");
+
+let index = 0;
 const myLibrary = [];
+const bookCardDiv = [];
 
 // Event Listeners
 
@@ -23,8 +27,10 @@ delAllBtn.addEventListener("click", () => {
     
 });
 confirmBtn.addEventListener("click", (event) => {
+    event.preventDefault();
     const newBook = new Book(titleUserInput.value, authorUserInput.value, urlUserInput.value, statusUserInput.checked);
     addBookToLibrary(newBook);
+    newBookDialog.close();
 });
 cancelBtn.addEventListener("click", () => {
     newBookDialog.close();
@@ -43,7 +49,29 @@ function Book (title, author, url, status) {
 
 function addBookToLibrary (book) {
     myLibrary.push(book);
-    console.log(myLibrary);
+
+    const div = document.createElement("div");
+    div.classList.add("card","book");
+    div.setAttribute("id", index);
+    if (book.url === "") {
+        const h2 = document.createElement("h2");
+        const p = document.createElement("p");
+        h2.textContent = book.title;
+        p.textContent = "by "+book.author;
+        div.appendChild(h2);
+        div.appendChild(p);
+    } else {
+        const img = document.createElement("img");
+        img.setAttribute("src", book.url);
+        img.setAttribute("alt", book.title+" by "+book.author);
+        img.setAttribute("title", book.title+" by "+book.author);
+        img.style.width = "170px";
+        img.style.height = "240px";
+        div.appendChild(img);
+    }
+    mainSection.appendChild(div);
+    bookCardDiv.push(div);
+    index++;
 }
 
 function deleteBookFromLibrary (book) {
